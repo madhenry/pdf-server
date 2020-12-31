fe95a6a484d5bf13f489cf643b91f3a4871423ee
Henry Kehlmann <henry@blooob.me>
2020-12-31 18:49:18 +0200

    Adds segment possibility

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,27 @@
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
 import { ColorModeScript } from '@chakra-ui/react'
+import * as snippet from '@segment/snippet'
 
+const {
+  NEXT_PUBLIC_SEGMENT_TOKEN
+} = process.env
 export default class Document extends NextDocument {
+  renderSnippet() {
+    const opts = {
+      apiKey: NEXT_PUBLIC_SEGMENT_TOKEN,
+      // note: the page option only covers SSR tracking.
+      // Page.js is used to track other events using `window.analytics.page()`
+      page: true,
+    }
+
+    return snippet.min(opts)
+  }
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          {NEXT_PUBLIC_SEGMENT_TOKEN && <script dangerouslySetInnerHTML={{ __html: this.renderSnippet() }} />}
+        </Head>
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
           <ColorModeScript initialColorMode="dark" />
